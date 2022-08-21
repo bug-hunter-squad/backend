7c544816e17e41d9fad152b3fbb663d18e5cb713
Mikhael <goodboybutnot@gmail.com>
2022-08-22 04:03:26 +0700

    testing status

--- app/controllers/paymentController.js
+++ app/controllers/paymentController.js
@@ -1,7 +1,8 @@
 const notifications = (req, res) => {
   const { transaction_status: transactionStatus, order_id: orderId } = req?.body;
+  console.log('transactionStatus, orderId', transactionStatus, orderId);
 
-  res.status(200).send({ transactionStatus, orderId });
+  res.status(200).send('Connected');
 };
 
 module.exports = { notifications };
