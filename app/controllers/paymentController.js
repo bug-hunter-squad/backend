3e00e9aebeaefcd36f31593fc42f3a7b64b8d662
Mikhael <goodboybutnot@gmail.com>
2022-08-22 04:23:24 +0700

    testing status

--- app/controllers/paymentController.js
+++ app/controllers/paymentController.js
@@ -1,6 +1,17 @@
-const notifications = (req, res) => {
-  const { transaction_status: transactionStatus, order_id: orderId } = req?.body;
-  console.log('transactionStatus, orderId', transactionStatus, orderId);
+const { flightBookingStatusModel } = require('../models/Flight');
+
+const notifications = async (req, res) => {
+  let { transaction_status: transactionStatus, order_id: orderId } = req?.body;
+
+  if (transactionStatus === 'pending') {
+    transactionStatus = 'waiting';
+    await flightBookingStatusModel({ transactionStatus, orderId });
+  }
+
+  if (transactionStatus === 'settlement ') {
+    transactionStatus = 'paid';
+    await flightBookingStatusModel({ transactionStatus, orderId });
+  }
 
   res.status(200).send('Connected');
 };
