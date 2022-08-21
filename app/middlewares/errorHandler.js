367aaeef02c52899e4afe8ef20d9ef710d44f32b
Mikhael <goodboybutnot@gmail.com>
2022-08-22 05:18:19 +0700

    fix(errorHandler): minor fix error handler

--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (error, req, res, next) => {
-  console.log('error', error);
+  // console.log('error', error);
   // Error List
   if (error?.code === '23505' && error?.constraint === 'users_email_key') {
     error.message = 'Email is already taken';
